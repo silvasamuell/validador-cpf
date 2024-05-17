7df48b6de61ec67e972fbe27b0e13c022de59ad2
Samuel Silva <sam.lucas.silva@gmail.com>
2024-05-16 23:55:25 -0300

    ajustando variaveis

--- src/app/ValidadorCPF.java
+++ src/app/ValidadorCPF.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class ValidadorCPF {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Scanner entrada = new Scanner(System.in);
 		
@@ -15,22 +14,22 @@ public class ValidadorCPF {
 		
 		int[] vetor = Arrays.stream(cpf.split("")).mapToInt(Integer::parseInt).toArray();
 		
-		int soma = 0;
+		int somaPrimeiroDigito = 0;
 		for (int i = 0; i < 9; i++) {
-		    soma += vetor[i] * (i + 1);
+			somaPrimeiroDigito += vetor[i] * (i + 1);
 		}
 		
-		int primeiroDigito = soma % 11;
+		int primeiroDigito = somaPrimeiroDigito % 11;
 		if(primeiroDigito == 10) {
 			primeiroDigito = 0;
 		}
 
-		int soma2 = 0;
+		int somaSegundoDigito = 0;
 		for (int i = 0; i < 10; i++) {
-		    soma2 += vetor[i] * (i);
+			somaSegundoDigito += vetor[i] * (i);
 		}
 		
-		int segundoDigito = soma2 % 11;
+		int segundoDigito = somaSegundoDigito % 11;
 		if(segundoDigito == 10) {
 			segundoDigito = 0;
 		}
