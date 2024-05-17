53215b432ac2e137bb6a1dfdada6fcc64650bd79
Samuel Silva <sam.lucas.silva@gmail.com>
2024-05-16 23:40:27 -0300

    trocando calculo pelo for

--- src/app/ValidadorCPF.java
+++ src/app/ValidadorCPF.java
@@ -10,37 +10,26 @@ public class ValidadorCPF {
 		
 		Scanner entrada = new Scanner(System.in);
 		
-		System.out.println("Digite o seu CPF (somente números)");
+		System.out.println("Digite o seu CPF");
 		String cpf = entrada.next();
 		
-		int[] dividir = Arrays.stream(cpf.split("")).mapToInt(Integer::parseInt).toArray(); 
+		int[] dividir = Arrays.stream(cpf.split("")).mapToInt(Integer::parseInt).toArray();
 		
-		int soma = (dividir[0] * 1) +
-				(dividir[1] * 2) +
-				(dividir[2] * 3) +
-				(dividir[3] * 4) +
-				(dividir[4] * 5) +
-				(dividir[5] * 6) +
-				(dividir[6] * 7) +
-				(dividir[7] * 8) +
-				(dividir[8] * 9);
+		int soma = 0;
+		for (int i = 0; i < 9; i++) {
+		    soma += dividir[i] * (i + 1);
+		}
 		
-		int resultado = soma % 11;
+		int primeiroDigito = soma % 11;
 
-		int soma2 = (dividir[0] * 0) +
-				(dividir[1] * 1) +
-				(dividir[2] * 2) +
-				(dividir[3] * 3) +
-				(dividir[4] * 4) +
-				(dividir[5] * 5) +
-				(dividir[6] * 6) +
-				(dividir[7] * 7) +
-				(dividir[8] * 8) +
-				(resultado * 9);
+		int soma2 = 0;
+		for (int i = 0; i < 10; i++) {
+		    soma2 += dividir[i] * (i);
+		}
 		
-		int resultado2 = soma2 % 11;
+		int segundoDigito = soma2 % 11;
 		
-		if (dividir[9] == resultado && dividir[10] == resultado2) {
+		if (dividir[9] == primeiroDigito && dividir[10] == segundoDigito) {
 			System.out.println("CPF VÁLIDO");
 		} else {
 			System.out.println("CPF INVÁLIDO");
