7fbf599f725baa64cddfd266e0de21267baf6170
Samuel Silva <sam.lucas.silva@gmail.com>
2024-05-16 23:50:25 -0300

    regra caso o resto da divisao de 10

--- src/app/ValidadorCPF.java
+++ src/app/ValidadorCPF.java
@@ -13,23 +13,29 @@ public class ValidadorCPF {
 		System.out.println("Digite o seu CPF");
 		String cpf = entrada.next();
 		
-		int[] dividir = Arrays.stream(cpf.split("")).mapToInt(Integer::parseInt).toArray();
+		int[] vetor = Arrays.stream(cpf.split("")).mapToInt(Integer::parseInt).toArray();
 		
 		int soma = 0;
 		for (int i = 0; i < 9; i++) {
-		    soma += dividir[i] * (i + 1);
+		    soma += vetor[i] * (i + 1);
 		}
 		
 		int primeiroDigito = soma % 11;
+		if(primeiroDigito == 10) {
+			primeiroDigito = 0;
+		}
 
 		int soma2 = 0;
 		for (int i = 0; i < 10; i++) {
-		    soma2 += dividir[i] * (i);
+		    soma2 += vetor[i] * (i);
 		}
 		
 		int segundoDigito = soma2 % 11;
+		if(segundoDigito == 10) {
+			segundoDigito = 0;
+		}
 		
-		if (dividir[9] == primeiroDigito && dividir[10] == segundoDigito) {
+		if (vetor[9] == primeiroDigito && vetor[10] == segundoDigito) {
 			System.out.println("CPF VÁLIDO");
 		} else {
 			System.out.println("CPF INVÁLIDO");
