fead0a39f58467f5d6db4814b82343373504894a
Samuel Silva <sam.lucas.silva@gmail.com>
2024-05-17 00:29:41 -0300

    indenta final

--- src/app/ValidadorCPF.java
+++ src/app/ValidadorCPF.java
@@ -6,21 +6,21 @@ import java.util.Scanner;
 public class ValidadorCPF {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entrada = new Scanner(System.in);
-		
+
 		System.out.println("Digite o seu CPF");
 		String cpf = entrada.next();
-		
+
 		int[] vetor = Arrays.stream(cpf.split("")).mapToInt(Integer::parseInt).toArray();
-		
+
 		int somaPrimeiroDigito = 0;
 		for (int i = 0; i < 9; i++) {
 			somaPrimeiroDigito += vetor[i] * (i + 1);
 		}
-		
+
 		int primeiroDigito = somaPrimeiroDigito % 11;
-		if(primeiroDigito == 10) {
+		if (primeiroDigito == 10) {
 			primeiroDigito = 0;
 		}
 
@@ -28,18 +28,18 @@ public class ValidadorCPF {
 		for (int i = 0; i < 10; i++) {
 			somaSegundoDigito += vetor[i] * (i);
 		}
-		
+
 		int segundoDigito = somaSegundoDigito % 11;
-		if(segundoDigito == 10) {
+		if (segundoDigito == 10) {
 			segundoDigito = 0;
 		}
-		
+
 		if (vetor[9] == primeiroDigito && vetor[10] == segundoDigito) {
 			System.out.println("CPF VÁLIDO");
 		} else {
 			System.out.println("CPF INVÁLIDO");
 		}
-		
+
 		entrada.close();
 	}
 
